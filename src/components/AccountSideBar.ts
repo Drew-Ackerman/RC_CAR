d453fc2c53f71a235f2191b8b7639f1c5388bcca
Drew <mitcha12@live.com>
2022-01-04 10:14:44 -0700

    Add comments, remove unused imports.

--- src/components/AccountSideBar.ts
+++ src/components/AccountSideBar.ts
@@ -1,14 +1,18 @@
-
-import { AccountSecurityPage } from "../pages/AccountSecurityPage";
 import { WebComponent } from "../lib/elements";
 import { findById } from "../lib/utils";
-import { pageHasLoaded } from "../lib/conditions";
 import { IBrowser } from "../interfaces/IBrowser";
 
+/**
+ * Options that can be selected on the side bar.
+ */
 export enum MenuOptions {
 	SecuritySettings="/account/View-Customer-Security",
 }
 
+/**
+ * This component is present on the side of all
+ * account pages. 
+ */
 export class AccountSideBar {
 	
 	@findById("sideBarMenu")
@@ -18,15 +22,12 @@ export class AccountSideBar {
 
 	}
 
+	/**
+	 * Select a menu option, this will change the account page you're on
+	 * @param option The menu option to click.
+	 */
 	public async selectMenuOption(option:MenuOptions): Promise<void>{
 		const menuOption = await this.sideBarMenu.findElement({css:`a[href='${option}']`});
 		await menuOption.click();
-
-		switch(option){
-		case MenuOptions.SecuritySettings:
-			//await this.browser.wait(pageHasLoaded(AccountSecurityPage));
-		}
 	}
-
-
 }
