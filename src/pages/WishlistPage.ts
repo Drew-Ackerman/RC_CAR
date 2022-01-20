217c73b183c4ab03fea4b40479b045095dfdab6a
Drew <mitcha12@live.com>
2022-01-20 15:04:16 -0700

    Add comments, add errors instead of prints, Change from variable looping to iterators.

--- src/pages/WishlistPage.ts
+++ src/pages/WishlistPage.ts
@@ -3,6 +3,9 @@ import { Browser, Button, elementIsVisible, findByCSS, findById, urlContainsValu
 import { ProductDetails } from "./ProductSearchPage";
 import { Page } from "../components/page";
 
+/**
+ * Defines important data the a wishlist item has. Useful when passing objects of data around.
+ */
 export type WishListDetails = {
 	productName:string,
 	productInfo:string,
@@ -22,7 +25,7 @@ export class WishListItem{
 	@findByCSS("a[class~='icon-cart']")
 	public moveToCartButton: Button;
 
-	//Find by is needs a local browser element, so for now we need one.
+	//Find by needs a local browser element, so for now we need one.
 	private browser;
 
 	constructor(private element: WebElement)
@@ -30,41 +33,54 @@ export class WishListItem{
 		this.browser = element;
 	}
 
-	public async productName(): Promise<string>{
+	/**
+	 * @returns The wishlist product name
+	 */
+	private async productName(): Promise<string>{
 		try{
 			return await this.productNameText.getText();
 		}catch(error){
-			console.error("Error getting wish list product name", error);
-			throw error;
+			throw new Error(`Error getting wishlist item's product name because of ${error}`);
 		}
 	}
 
-	public async productInfo(): Promise<string>{
+	/**
+	 * @returns The wishlist product info
+	 */
+	private async productInfo(): Promise<string>{
 		try{
 			return await this.productInfoText.getText();
 		}catch(error){
-			console.error("Error gettting wish list item product info", error);
-			throw error;
+			throw new Error(`Error getting wishlist item's product info because of ${error}`);
 		}
 	}
 
+	/**
+	 * @returns A wishlist item's product details.
+	 */
 	public async productDetails(): Promise<WishListDetails>{
 		try{
 			const name = await this.productName();
 			const info = await this.productInfo();
 			return {productName:name, productInfo:info};
 		} catch(error){
-			console.error(error);
-			throw error;
+			throw new Error(`Error getting wish list product details because of ${error}`);
+
 		}
 	}
 
+	/**
+	 * Remove this wishlist item from the wish list.
+	 */
 	public async deleteItem(): Promise<void>{
 		const trashcanBtn = new Button(this.element.findElement({className:"removeItemBtn"}), "removeItemBtn");
 		await trashcanBtn.click();
 	}
 }
 
+/**
+ * The wishlist page.
+ */
 export class WishlistPage extends Page {
 	
 	@findById("shoppingCart")
@@ -80,17 +96,26 @@ export class WishlistPage extends Page {
 	
 	public loadCondition(): WaitCondition {
 		return urlContainsValue(this.browser, "Shopping-Cart?listName=");
-		//return elementIsVisible(()=>this.shoppingCartHeader);
 	}
 
+	/**
+	 * Determines if the wishlist is empty
+	 * @returns True if empty, otherwise false. 
+	 */
 	public async wishlistIsEmpty(): Promise<boolean>{
-		const el = await this.browser.findElement({id:"emptyCart"});
-		if(await el.isDisplayed()){
+		const emptyCartElement = await this.browser.findElement({id:"emptyCart"});
+		if(await emptyCartElement.isDisplayed()){
 			return true;
 		}
 		return false;
 	}
 
+	/**
+	 * Get a list of wishlist items in the wishlist
+	 * Filters out the first wishlist item, as its assumed to be the membership advertisement.
+	 * @returns All wishlist items.
+	 * @throws An error if there are no wishlist items on the page.
+	 */
 	public async getWishlistItems(): Promise<Array<WishListItem>>{
 		await this.browser.wait(elementIsVisible(() => this.shoppingCart));
 		const items = await this.shoppingCart.findElements({css:"div[class~='cartItem']"});
@@ -104,10 +129,15 @@ export class WishlistPage extends Page {
 		return wishlistItems;
 	}
 
+	/**
+	 * See if a product is in the wishlist
+	 * @param product The product details of the product to search for.
+	 * @returns True if present, false otherwise.
+	 */
 	public async checkProductIsInWishlist(product:ProductDetails): Promise<boolean>{
 		const items = await this.getWishlistItems();
-		for(let i = 0; i < items.length; i++){
-			const wishlistProductName = (await items[i].productDetails()).productName;
+		for(const item of items){
+			const wishlistProductName = (await item.productDetails()).productName;
 			if(product.productName === wishlistProductName){
 				return true;
 			}
@@ -115,25 +145,37 @@ export class WishlistPage extends Page {
 		return false;
 	}		
 
-	public async removeProductFromWishlist(product:ProductDetails): Promise<void>{
+	/**
+	 * Remove a product from the wishlist
+	 * @param product The product to remove
+	 * @returns Return the product details of a successfully removed product.
+	 * @throws An error if no item is deleted from the wishlist.
+	 */
+	public async removeProductFromWishlist(product:ProductDetails): Promise<WishListItem>{
 		const items = await this.getWishlistItems();
-		for(let i=0; i < items.length; i++){
-			const itemDetails = await items[i].productDetails();
+		for(const item of items){
+			const itemDetails = await item.productDetails();
 			if(product.productName == itemDetails.productName){
-				await items[i].deleteItem();
-				return;
+				await item.deleteItem();
+				return item;
 			}
 		}
 		throw new Error(`Product: ${product.productName} was not found in wishlist to delete`);
 	}
 
-	public async addWishlistItemToCart(product:ProductDetails): Promise<void>{
+	/**
+	 * Add a wislist item to the cart
+	 * @param product The product to add to the cart
+	 * @returns The item if successful
+	 * @throws An error if the wanted product is not present in the wishlist.
+	 */
+	public async addWishlistItemToCart(product:ProductDetails): Promise<WishListItem>{
 		const items = await this.getWishlistItems();
-		for(let i=0; i < items.length; i++){
-			const itemsDetails = await items[i].productDetails();
+		for(const item of items){
+			const itemsDetails = await item.productDetails();
 			if(product.productName == itemsDetails.productName){
-				await items[i].moveToCartButton.click();
-				return;
+				await item.moveToCartButton.click();
+				return item;
 			}
 		}
 		throw new Error(`Could not add ${product.productName} from wishlist to shopping cart page`);
