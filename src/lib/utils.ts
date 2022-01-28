73d4e1c2457729572207be65a7bec09cd3b0ee67
Drew <mitcha12@live.com>
2022-01-28 12:50:14 -0700

    Change names, and comments.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,26 +3,26 @@ import { ByHash } from "selenium-webdriver";
 
 /**
  * When this code is used inside a property decorator what is happening is a class property 
- * is created. The property will be of whatever type is given to typescript via: 
+ * is created. The property will be of whatever type is given via typescript.
  * 
  * So when used like 
  * findById("myId")
- * private myPageElement: Button
+ * private myPageComponent: Button
  * 
  * A property is attached to the POM class where the name is 
- * myPageElement, it is of type Button, and then a get function is 
- * created that returns a NEW Button.
+ * myPageComponent, it is of type Button, and then a get function is 
+ * created that returns a NEW Button component when called.
  * 
  * This promise part is important. It means that when a Page Object Model (POM) is created, 
- * that the elements of those pages are defined, but those elements are not searched for 
- * UNTIL the property is used. So everytime a POM property is used like myPageElement.click(), the element on the page 
+ * that the components of those pages are defined, but those componentes are not searched for 
+ * UNTIL the property is used. So everytime a POM property is used like myPageComponent.click(), the component on the page 
  * is searched for, again.
  * 
  * @param target The class being effected. 
  * @param propertyKey The name of the property to change
- * @param selector A find by selector for the element
+ * @param selector A find by selector for the component
  */
-function elementFactory(target: any, propertyKey: string, selector: ByHash){
+function componentFactory(target: any, propertyKey: string, selector: ByHash){
 	const selectorString = `By: ${Object.keys(selector)[0]}, On: ${Object.values(selector)[0]}`;
 	const type = Reflect.getMetadata("design:type", target, propertyKey);
 	Object.defineProperty(target, propertyKey, {
@@ -33,35 +33,35 @@ function elementFactory(target: any, propertyKey: string, selector: ByHash){
 			return new type(promise, selectorString);
 		},
 	});
-};
+}
 
 /**
  * Use as a decorator on a WebComponent property of a POM
- * Find an element by their css ID
+ * Find a component by their css ID
  * @param idSelector The ID 
  * @returns Nothing. 
  */
 export function findById(idSelector: string){
 	return (target: any, propertyKey: string) => {
-		elementFactory(target, propertyKey, {id:`${idSelector}`});
+		componentFactory(target, propertyKey, {id:`${idSelector}`});
 	};
 }
 
 /**
  * Use as a decorator on a WebComponent property of a POM
- * Find an element by their css class 
+ * Find a component by their css class 
  * @param selector The css class name selector
  * @returns Nothing. 
  */
 export function findByClass(selector: string){
 	return (target: any, propertyKey: string) => {
-		elementFactory(target, propertyKey, {className:`${selector}`});
+		componentFactory(target, propertyKey, {className:`${selector}`});
 	};
 }
 
 /**
  * Use as a decorator on a WebComponenet propery of a POM
- * Find all elements by their css class.
+ * Find all components by their css class.
  * @param selector The selector used to find all available webComponents
  * @returns Nothing.
  */
@@ -81,61 +81,61 @@ export function findAllByClass(selector: string){
 
 /**
  * Use as a decorator on a WebComponent property of a POM
- * Find an element by their _visible_ link text, used on <a> tags. 
+ * Find an component by their _visible_ link text, used on <a> tags. 
  * @param linkText The links tags text
  * @returns Nothing. 
  */
 export function findByLinkText(linkText: string){
 	return (target: any, propertyKey: string) => {
-		elementFactory(target, propertyKey, {linkText:`${linkText}`});
+		componentFactory(target, propertyKey, {linkText:`${linkText}`});
 	};
 }
 
 /**
  * Use as a decorator on a WebComponent property of a POM
- * Find an element by the class name of the element.
- * @param nameValue The css class name of the element
+ * Find an component by the class name of the component.
+ * @param nameValue The css class name of the component
  * @returns Nothing. 
  */
 export function findByName(nameValue: string){
 	return (target: any, propertyKey: string) => {
-		elementFactory(target, propertyKey, {name:`${nameValue}`});
+		componentFactory(target, propertyKey, {name:`${nameValue}`});
 	};
 }
 
 /**
  * Use as a decorator on a WebComponent property of a POM
- * Find an element by their _visible_ link text, used on <a> tags. 
+ * Find an component by their _visible_ link text, used on <a> tags. 
  * @param linkText The links tags text
  * @returns Nothing. 
  */
 export function findByPartialLinkText(linkText: string){
 	return (target: any, propertyKey: string) => {
-		elementFactory(target, propertyKey, {partialLinkText:`${linkText}`});
+		componentFactory(target, propertyKey, {partialLinkText:`${linkText}`});
 	};
 }
 
 /**
  * Use as a decorator on a WebComponent property of a POM
- * Find an element by the css of the element. Useful for finding 
+ * Find an component by the css of the component. Useful for finding 
  * html tags as well - e.g. <div>
  * @param cssIdentifier The css identifier
  * @returns 
  */
 export function findByCSS(cssIdentifier: string){
 	return (target: any, propertyKey: string) => {
-		elementFactory(target, propertyKey, {css:`${cssIdentifier}`});
+		componentFactory(target, propertyKey, {css:`${cssIdentifier}`});
 	};
 }
 
 /**
  * Use as a decorator on a WebComponent property of a POM
- * Find an element by an xpath.
- * @param xpath The xpath to find the element
+ * Find an component by an xpath.
+ * @param xpath The xpath to find the component
  * @returns Nothing. 
  */
 export function findByXpath(xpath: string){
 	return (target: any, propertyKey: string) => {
-		elementFactory(target, propertyKey, {xpath:`${xpath}`});
+		componentFactory(target, propertyKey, {xpath:`${xpath}`});
 	};
 }
